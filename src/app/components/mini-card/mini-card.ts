a49610865eaef879adca3d007c6194b95fbec3da
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2026-04-19 10:37:23 -0300

    feat: minicard

--- src/app/components/mini-card/mini-card.ts
+++ src/app/components/mini-card/mini-card.ts
@@ -6,21 +6,26 @@ import { MatIcon } from "@angular/material/icon";
   selector: 'app-mini-card',
   imports: [CurrencyPipe, MatIcon],
   template: `
-    <div class="w-[200px]">
-      <div class="flex justify-between items-center gap-10 bg-gray-100 rounded-2xl p-4 shadow shadow-gray-200">
-        <span class="font-normal text-blue-900 text-shadow text-shadow-2xs text-shadow-gray-200">
+    <div
+      class="w-[256px] h-[128px] flex justify-between items-center gap-10 bg-gray-100 rounded-3xl px-8 group hover:bg-gray-200 transition-colors duration-100"
+    >
+      <div>
+        <p class="text-lg font-bold text-gray-500 text-shadow tracking-wider">Saldo</p>
+        <span class="text-sm font-normal text-gray-500 text-shadow tracking-wider">
           {{ valor() | currency: 'BRL' }}
         </span>
-        <div class="flex items-center bg-blue-200 p-2 rounded-lg">
-          <mat-icon class="!text-blue-900">{{ icon() }}</mat-icon>
-        </div>
+      </div>
+      <div
+        class="flex items-center bg-[#c2e7ff] p-2 group-hover:bg-blue-900 rounded-lg transition-colors duration-100"
+      >
+        <mat-icon
+          class="!text-blue-900 group-hover:!text-[#c2e7ff] transition-colors duration-100"
+          >{{ icon() }}</mat-icon
+        >
       </div>
     </div>
   `,
   styles: `
-    :host {
-      display: block;
-    }
   `,
 })
 export class MiniCard {
