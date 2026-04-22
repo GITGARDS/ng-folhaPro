025ce7188406d7d31b59e9483a4cb597503d2533
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2026-04-21 23:12:48 -0300

    feat: teste de versao

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from "@angular/core";
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from "@angular/core";
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 import { provideRouter, withViewTransitions } from "@angular/router";
 import { provideEnvironmentNgxMask } from "ngx-mask";
-import { environment } from "../environments/environment";
+import { environment } from "../environments/environment.development";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+
     provideRouter(routes, withViewTransitions()),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from "@angular/core";
+import { Component } from "@angular/core";
 import { Navigation } from "./app/navigation/navigation";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [Navigation],
   template: ` <app-navigation /> `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('ng-folhaPro');
+  protected title = "ng-folhaPro";
 }
